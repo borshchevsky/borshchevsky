dd879e7b2765608691c24c2088d2ee41a64fc95d
Pavel <3410914@gmail.com>
2021-03-26 18:12:05 +0300

    2_1 changes resolved

--- market/main/models.py
+++ market/main/models.py
@@ -19,7 +19,6 @@ class Seller(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    seller = models.ManyToManyField(Seller)
     title = models.CharField(max_length=255)
     width = models.IntegerField(blank=True)
     height = models.IntegerField(blank=True)
@@ -28,3 +27,14 @@ class Product(models.Model):
 
     def __repr__(self):
         return f'{self.__class__.__name__}({self.title})'
+
+
+class ProductInstance(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.IntegerField(null=True)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}{self.product.title, self.price}'
+
