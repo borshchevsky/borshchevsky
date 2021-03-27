87786aedb23dc4c7e367e4da03af9af4623344be
Pavel <3410914@gmail.com>
2021-03-27 16:53:23 +0300

    3_1 done

--- market/main/views.py
+++ market/main/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    turn_on_block = True
+    return render(request, 'index.html', {'turn_on_block': turn_on_block})
