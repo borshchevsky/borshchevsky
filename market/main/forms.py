71d3e9bb48c6f606255dda9019c899dabd9f67ca
Pavel Borshchevsky <3410914@gmail.com>
2021-04-01 17:28:05 +0300

    4_2_1 done

--- market/main/forms.py
+++ market/main/forms.py
@@ -1,13 +1,22 @@
 from django import forms
+from django.contrib.auth.models import User
 
 from .models import Profile
 
 
 class ProfileForm(forms.ModelForm):
+    ...
+
+
+class UserForm(forms.ModelForm):
     class Meta:
-        model = Profile
-        fields = [
+        model = User
+        fields = (
             'first_name',
             'last_name',
             'email',
-        ]
+        )
+
+
+ProfileFormSet = forms.inlineformset_factory(User, Profile, fields='__all__', extra=0, min_num=1,
+                                             can_delete=False)
