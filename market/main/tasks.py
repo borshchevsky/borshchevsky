3156770fa7a151497e2ff81c33ae3ae9e467d256
Pavel <3410914@gmail.com>
2021-04-13 11:37:30 +0300

    7_3 done

--- market/main/tasks.py
+++ market/main/tasks.py
@@ -1,11 +1,9 @@
+from celery.schedules import crontab
+
+from main.utils import send_novelty_weekly
 from market.celery import celery_app
 
 
-@celery_app.on_after_configure.connect
+@celery_app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(3.0, test.s())
-
-
-@celery_app.task
-def test():
-    print('test')
+    sender.add_periodic_task(crontab(hour=1, day_of_week='sat'), send_novelty_weekly.s())
--- market/main/views.py
+++ market/main/views.py
@@ -135,25 +135,3 @@ def send_novelty(instance, **kwargs):
         fail_silently=False,
         html_message=message
     )
-
-
-def send_novelty_weekly():
-    now = timezone.now().date()
-    week_ago = now - timedelta(7)
-    novelty_count = len(Product.objects.filter(date_created__range=[week_ago, now]))
-
-    message = f'''
-        <h3>We have some novelties.</h3>
-        You received this message because you have subscribed for information about new goods.
-        <br>
-        We got {novelty_count} novelties last week! Take a look!
-        '''
-    mail_list = {sub.user.email for sub in Subscriber.objects.all() if sub.user.email}
-    send_mail(
-        subject='A new good has just arrived.',
-        message='Hello.',
-        from_email='admin@example.com',
-        recipient_list=mail_list,
-        fail_silently=False,
-        html_message=message
-    )
