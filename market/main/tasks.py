e34bc45dad65c991298cdfb4e1263ad29e22e487
Pavel <3410914@gmail.com>
2021-04-13 17:25:59 +0300

    minor changes

--- market/main/tasks.py
+++ market/main/tasks.py
@@ -1,8 +1,10 @@
+from datetime import timedelta
+
 from celery.schedules import crontab
 from django.core.mail import send_mail
+from django.utils import timezone
 
 from main.models import Subscriber, Product
-from main.utils import send_novelty_weekly
 from market.celery import celery_app
 
 
@@ -24,6 +26,29 @@ def send_novelty_task(product_id):
         <a href="http://127.0.0.1:8000/goods/{product.id}>{product.title}</a>
         '''
 
+    mail_list = {sub.user.email for sub in Subscriber.objects.all() if sub.user.email}
+    send_mail(
+        subject='A new good has just arrived.',
+        message='Hello.',
+        from_email='admin@example.com',
+        recipient_list=mail_list,
+        fail_silently=False,
+        html_message=message
+    )
+
+
+@celery_app.task
+def send_novelty_weekly():
+    now = timezone.now().date()
+    week_ago = now - timedelta(7)
+    novelty_count = len(Product.objects.filter(date_created__range=[week_ago, now]))
+
+    message = f'''
+        <h3>We have some novelties.</h3>
+        You received this message because you have subscribed for information about new goods.
+        <br>
+        We got {novelty_count} novelties last week! Take a look!
+        '''
     mail_list = {sub.user.email for sub in Subscriber.objects.all() if sub.user.email}
     send_mail(
         subject='A new good has just arrived.',
