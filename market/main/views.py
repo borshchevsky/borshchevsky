553782a9392e70e79b298fa639bc22ccc0b5cde1
Pavel <3410914@gmail.com>
2021-04-14 12:08:17 +0300

    7_5 done 2.1

--- market/main/views.py
+++ market/main/views.py
@@ -1,6 +1,5 @@
 from django.contrib import messages
 from django.contrib.auth.models import User, Group
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
