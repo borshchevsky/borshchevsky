0eb5220fc11103d0f4390daf38771e2adbf2710f
Pavel <3410914@gmail.com>
2021-04-14 15:26:46 +0300

    8_3 done

--- market/main/urls.py
+++ market/main/urls.py
@@ -10,7 +10,7 @@ from .validators import group_check
 urlpatterns = [
                   path('', views.index, name='index'),
                   path('goods/', views.ProductListView.as_view(), name='goods'),
-                  path('goods/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+                  path('goods/<int:pk>', (views.ProductDetailView.as_view()), name='product-detail'),
                   path('accounts/profile/', login_required(views.ProfileUpdate.as_view()),
                        name='profile-update'),
                   path('goods/add', group_check(views.CreateProduct.as_view())),
